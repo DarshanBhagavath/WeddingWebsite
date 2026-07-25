23beff801885ec9a6e7e085f485e93e6f20d1491
Darshan Bhagavath <85301722+DarshanBhagavath@users.noreply.github.com>
2026-07-24 22:29:18 -0400

    afdg

--- server.ts
+++ server.ts
@@ -25,7 +25,8 @@ async function startServer() {
   const app = express();
   const PORT = 3000;
 
-  app.use(express.json());
+  app.use(express.json({ limit: '500mb' }));
+  app.use(express.urlencoded({ limit: '500mb', extended: true }));
   app.use('/uploads', express.static(UPLOADS_DIR));
 
   let mediaConfig = {
